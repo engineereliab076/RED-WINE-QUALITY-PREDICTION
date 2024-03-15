13c64b26fab783d2ece97db3fe2093d2bbf10e2e
engineereliab076 <eliabujohnmpuya@gmail.com>
2024-03-15 23:28:39 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Red_wine import logger
 from Red_wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Red_wine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from Red_wine.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -24,3 +24,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
