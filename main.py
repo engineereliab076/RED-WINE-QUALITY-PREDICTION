75e35365aa1bae8b58dd3828d578fed3d03e7738
engineereliab076 <eliabujohnmpuya@gmail.com>
2024-03-15 22:48:26 +0530

    data validation

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from Red_wine import logger
 from Red_wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Red_wine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/Red_wine/pipeline/stage_02_data_validation.py
@@ -0,0 +1,27 @@
+from Red_wine.config.configuration import ConfigurationManager
+from Red_wine.components.data_validation import DataValidation
+from Red_wine import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
