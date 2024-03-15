2b52bd72b11a571e17c1af86d3d12672b62c7d4e
engineereliab076 <eliabujohnmpuya@gmail.com>
2024-03-15 22:14:29 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from Red_wine import logger
+from Red_wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("my loggings")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
